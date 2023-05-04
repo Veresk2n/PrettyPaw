cf58f28b44b33eea3863df814d320c8b05ff53f7
Veresk2n <veresk2n@mail.ru>
2023-05-04 16:24:58 +0500

    ready layout wo career

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.html';
+// new modules
+import './index.scss';
+
+
+//use modules
---
+++ webp-convert.mjs
@@ -0,0 +1,11 @@
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+
+await imagemin(['src/img/*.{jpg,png}'], {
+    destination: 'src/img',
+    plugins: [
+        imageminWebp({quality: 70}),
+    ]
+});
+
+console.log('Conversion into webp completed')
