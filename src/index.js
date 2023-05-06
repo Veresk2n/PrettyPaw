74eebd3413196cc5dd8581212f23eb1b58efc93d
veresk2n <veresk2n@mail.ru>
2023-05-06 16:53:22 +0500

    add style to hero section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './index.html';
 // new modules
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
 
 
 //use modules
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+    <source src="video/video.webm" type="video/webm">
+    <source src="video/video.mp4" type="video/mp4">
+`;
